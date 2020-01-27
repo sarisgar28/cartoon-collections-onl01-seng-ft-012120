492d1c79f8c42361fe4921ee7ee71deaadab2b06
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 22:20:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves = ["doc","dopey","bashful","grumpy"]
+  puts "Hi Snow white, my name is #{name}!"
+  
 end
 
 def summon_captain_planet# code an argument here
